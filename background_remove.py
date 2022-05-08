261fd4c98b970237049bac9fbeed1d5831a428d0
Sarthak Bajaj <46028985+SarthakB16@users.noreply.github.com>
2022-05-08 22:57:40 +0530

    Update background_remove.py

--- background_remove.py
+++ background_remove.py
@@ -4,7 +4,7 @@ response = requests.post(
     'https://api.remove.bg/v1.0/removebg',
     files={'image_file': open('D:\intern\intern.png', 'rb')},
     data={'size': 'auto', 'bg':'white','bg_type':'color'},
-    headers={'X-Api-Key': 'mWZMowVkUHWqvuxRe1KqWnVB'},
+    headers={'X-Api-Key': 'Using_your_key_here'},
 )
 if response.status_code == requests.codes.ok:
     with open('no-bg.png', 'wb') as out:
