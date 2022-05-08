3264e61d55a4c006e2be51c2e6267b69eed67881
Sarthak Bajaj <46028985+SarthakB16@users.noreply.github.com>
2022-05-08 22:53:36 +0530

    Add files via upload

---
+++ add_white_background.py
@@ -0,0 +1,27 @@
+
+import numpy as np
+import cv2
+from PIL import Image
+
+# creating array using np.zeroes()
+array = np.zeros([500, 500, 3],
+                 dtype = np.uint8)
+  
+# setting RGB color values as 255,255,255
+array[:, :] = [255, 255, 255] 
+  
+# displaying the image
+cv2.imshow("image", array)
+cv2.waitKey(0)
+cv2.imwrite('white-bg.png',array)
+
+
+img1 = Image.open(r"white-bg.png")
+img2 = Image.open(r"no-bg.png")
+  
+# Pasting img2 image on top of img1 
+# starting at coordinates (0, 0) 
+# simulating an raster overlay
+img1.paste(img2, (0,0), mask = img2)
+  
+img1.show()
---
+++ background_remove.py
@@ -0,0 +1,13 @@
+from urllib import response
+import requests
+response = requests.post(
+    'https://api.remove.bg/v1.0/removebg',
+    files={'image_file': open('D:\intern\intern.png', 'rb')},
+    data={'size': 'auto', 'bg':'white','bg_type':'color'},
+    headers={'X-Api-Key': 'mWZMowVkUHWqvuxRe1KqWnVB'},
+)
+if response.status_code == requests.codes.ok:
+    with open('no-bg.png', 'wb') as out:
+        out.write(response.content)
+else:
+    print("Error:", response.status_code, response.text)
